0adc1cae2540c1b4e861f7d2cfe8a6a7206126d2
Limara Demerdzhy <limara.demerdzhy@gmail.com>
2025-03-12 21:52:54 +0000

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,15 @@
  */
 function calculateRentalCost(days) {
   const baseCost = days * 40;
+  const discountOne = 20;
+  const discountTwo = 50;
 
   if (days >= 3 && days < 7) {
-    return baseCost - 20;
+    return baseCost - discountOne;
   }
 
   if (days >= 7) {
-    return baseCost - 50;
+    return baseCost - discountTwo;
   }
 
   return baseCost;
